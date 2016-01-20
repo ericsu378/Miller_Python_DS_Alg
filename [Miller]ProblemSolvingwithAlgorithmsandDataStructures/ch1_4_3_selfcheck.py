d00074580226790d065212a10ef36cec88896c5f
Eric Su <su.eric.09@gmail.com>
2016-01-19 22:44:12 -0800

    ch1.4.3 selfcheck

---
+++ [Miller]ProblemSolvingwithAlgorithmsandDataStructures/ch1_4_3_selfcheck.py
@@ -0,0 +1,47 @@
+__author__ = 'ESU_2'
+
+# Problem Solving with Algorithms and Data Structures Release 3.0
+# Brad Miller, David Ranum
+# September 22, 2013
+
+# 1.4.3 Control Structure Self Check
+
+# Original Code
+# word_list = ['cat','dog','rabbit']
+# letter_list = []
+# for a_word in word_list:
+#     for a_letter in a_word:
+#         letter_list.append(a_letter)
+# print(letter_list)
+
+# 1. Modify the given code so that the final list only contains a single copy of each letter.
+#  the answer is: ['c', 'a', 't', 'd', 'o', 'g', 'r', 'b', 'i']
+# word_list = ['cat','dog','rabbit']
+# letter_list = []
+# for a_word in word_list:
+#     for a_letter in a_word:
+#         if a_letter not in letter_list:
+#             letter_list.append(a_letter)
+# print(letter_list)
+
+
+#2. Redo the given code using list comprehensions.
+# For an extra challenge, see if you can figure out how to remove the duplicates.
+# the answer is: ['c', 'a', 't', 'd', 'o', 'g', 'r', 'a', 'b', 'b', 'i', 't']
+word_list = ['cat','dog','rabbit']
+letter_list = [a_letter for a_word in word_list for a_letter in a_word]
+print(letter_list)
+
+# Approach 1
+unique = []
+[unique.append(a_letter) for a_word in word_list for a_letter in a_word if a_letter not in unique]
+print(unique)
+
+# Approach 2, using sets
+unique2 = {a_letter for a_word in word_list for a_letter in a_word}
+print unique2
+print list(unique2)
+
+# Turn word_list into sets
+
+# union each item
